d129c59e60941c0f507e3f885fdf5036bb660630
rhuthuparna sj <rithuparna736@gmail.com>
2026-04-22 12:48:53 +0530

    Create addtwonumbers.c

---
+++ addtwonumbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Sum = %d", a + b);
+
+    return 0;
+}
+output
+Enter two numbers: 2
+3
+Sum = 5
+
+=== Code Execution Successful ===
